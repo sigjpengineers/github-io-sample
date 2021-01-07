e6ab3c245454851fa7bdb1b010866b1a4e09de5c
sigjpengineers <67721801+sigjpengineers@users.noreply.github.com>
2021-01-07 11:44:26 +0900

    Update AuthenticationController.java

--- src/main/java/com/example/controllers/AuthenticationController.java
+++ src/main/java/com/example/controllers/AuthenticationController.java
@@ -20,7 +20,7 @@ public class AuthenticationController {
 	@RequestMapping("/403")
 	public String error403(final Model model) {
 		System.out.println(username);
-		System.out.println("Error 403");
+		//System.out.println("Error 403");
 		return "error403";
 	}
 }
